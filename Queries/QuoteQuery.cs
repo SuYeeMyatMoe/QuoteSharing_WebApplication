dc353e17b5a8207ba509546d9c840827e229e6e4
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-01-25 16:08:30 +0630

    Update Controller

--- Queries/QuoteQuery.cs
+++ Queries/QuoteQuery.cs
@@ -18,5 +18,9 @@
         public static string GetQuoteByIdQuery { get; } =
         @"SELECT QuoteID, QuoteWriter, QuoteText, UploadedEmail, IsDeleted
 FROM Tbl_QuoteSharing WHERE IsDeleted = @IsDeleted AND QuoteID = @QuoteID";
+
+        public static string UpdateQuoteQuery { get; } =
+        @"UPDATE Tbl_QuoteSharing SET QuoteWriter = @QuoteWriter, QuoteText = @QuoteText,
+UploadedEmail = @UploadedEmail WHERE QuoteID = @QuoteID AND IsDeleted = @IsDeleted";
     }
 }
